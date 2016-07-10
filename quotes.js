a8f03e5177f77ac22b86870919d4b031f6a8b764
Piotr <piotrryszewski@gmail.com>
2016-07-10 14:20:13 +0200

    Finished
    Used jQuery to finally get it to work. 
    
    There is a long way ahead of me.

--- quotes.js
+++ quotes.js
@@ -1,8 +1,11 @@
 
-var quotesArr = ["When you arise in the morning, think of what a precious privilege it is to be alive - to breathe, to think, to enjoy, to love. - Marcus Aurelius", "Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth. - Marcus Aurelius", "Let men see, let them know, a real man, who lives as he was meant to live. - Marcus Aurelius", "I've got balls of steel! - Duke Nukem","Don't have time to play with myself. - Duke Nukem"  ]
+var quotesArr = ["When you arise in the morning, think of what a precious privilege it is to be alive - to breathe, to think, to enjoy, to love. - Marcus Aurelius", "Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth. - Marcus Aurelius", "Let men see, let them know, a real man, who lives as he was meant to live. - Marcus Aurelius", "I've got balls of steel! - Duke Nukem","Don't have time to play with myself. - Duke Nukem"  ];
 
-function generateQuote(){
-var random = quotesArr[Math.floor(Math.random() * quotesArr.length)];
-document.getElementById("quote-placement").innerHTML(random);
 
-  }
+$(document).ready(function() {
+
+$("#quote-button").click(function() {
+var item = quotesArr[Math.floor(Math.random() * quotesArr.length)];
+$("#quote-placement").html(item);
+});
+});
